1fc8540eb6049688bf9c464c51c25e7932822360
Mehigh <mihai.tirtara@gmail.com>
2020-07-14 17:06:11 +0200

    Unnecessary files removed

--- python_separate.py
+++
@@ -1,9 +0,0 @@
-import ijson
-
-events = ijson.basic_parse(open("/home/mehigh/AddressData/AddressAgain.json"))
-for prefix,event, value in events:
-    if(prefix,event) == ('AdresseList','map_key'):
-       print(value)
-
-
-
